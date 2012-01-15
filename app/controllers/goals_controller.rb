cbe6ec4c608575d6c02b40360952556925b6d2a7
Jeff Escalante <hello@jenius.me>
2012-01-15 11:41:23 -0500

    fix redirect after goal creation and disallow users from editing other users' goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,45 +1,55 @@
 class GoalsController < ApplicationController
 
-	def index
-		@user = User.find(params[:user_id])
-		@goals = Goal.all
-	end
+  before_filter :authenticate_user!, :match_user
 
-	def new
-		@user = User.find(params[:user_id])
-		@goal = @user.goals.new(params[:goal])
-	end
+  def match_user
+    if current_user.id.to_i != params[:user_id].to_i
+      redirect_to root_path
+      flash[:notice] = "You can't edit another user's goals!"
+    end
+  end
 
-	def create
-		params[:goal][:state] = :pending
-		@user = User.find(params[:user_id])
-		@goal = @user.goals.new(params[:goal])
-		if @goal.save
-			redirect_to user_goals_path(1)
-		else
-			render 'new'
-		end
-	end
+  def index
+    @user = User.find(params[:user_id])
+    @goals = Goal.all
+    logger.debug "=========================="
+  end
 
-	def show
-		@user = User.find(params[:user_id])
-		@goal = @user.goals.find(params[:id])
-	end
+  def new
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.new(params[:goal])
+  end
 
-	def edit
-		@user = User.find(params[:user_id])
-		@goal = @user.goals.find(params[:id])
-		logger.debug @goal.inspect
-	end
+  def create
+    params[:goal][:state] = :pending
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.new(params[:goal])
+    if @goal.save
+      redirect_to user_goals_path(params[:user_id])
+    else
+      render 'new'
+    end
+  end
 
-	def update
-		@user = User.find(params[:user_id])
-		@goal = @user.goals.find(params[:id])
-		if @goal.update_attributes(params[:goal])
-			redirect_to user_goal_path(@user, @goal)
-		else
-			render "edit"
-		end
-	end
+  def show
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.find(params[:id])
+    logger.debug @goal.inspect
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @goal = @user.goals.find(params[:id])
+    if @goal.update_attributes(params[:goal])
+      redirect_to user_goal_path(@user, @goal)
+    else
+      render "edit"
+    end
+  end
 
 end
