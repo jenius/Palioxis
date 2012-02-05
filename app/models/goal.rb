795a99eb473e2827ebd359612a1479871371b89c
Jeff Escalante <hello@jenius.me>
2012-02-05 01:29:03 -0500

    rake tasks working well, scopes updated to acommodate charged goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,8 +3,8 @@ class Goal < ActiveRecord::Base
   attr_accessible :name, :due, :created_at, :payment, :emails, :other, :state
 
   scope :pending, lambda { where("state = 'pending'") }
-  scope :failed, lambda { where("state = 'failed'") }
+  scope :failed, lambda { where("state = 'failed' OR state = 'charged'") }
   scope :completed, lambda { where("state = 'completed'") }
-  scope :done, lambda { where("state = 'failed' OR state = 'completed'") }
+  scope :done, lambda { where("state = 'failed' OR state = 'charged' OR state = 'completed'") }
 
 end
