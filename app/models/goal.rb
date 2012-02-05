ccc075ccf6714b3d67ee244485c98c507a4253a7
Jeff Escalante <hello@jenius.me>
2012-02-05 00:42:08 -0500

    scoped goals to pending and completed

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,8 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name, :due, :created_at, :payment, :emails, :other, :state
+
+  scope :pending, lambda { where("state = 'pending'") }
+  scope :completed, lambda { where("state = 'failed' OR state = 'completed'") }
+
 end
