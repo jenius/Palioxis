96a8695ff70406abe2383db39293fd253b67dcdc
Jeff Escalante <hello@jenius.me>
2012-02-07 23:31:07 -0500

    time until due

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def time_until_due(due)
+    if due > Date.today
+      if due == Date.tomorrow
+        "due tomorrow"
+      else
+        "due in #{time_ago_in_words(due)}"
+      end
+    else
+      "overdue!"
+    end
+  end
+
 end
